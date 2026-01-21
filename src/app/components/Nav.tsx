a38278b544da2852364029e9d53edb6402d489ac
Bastiabm18 <andresbarriosmedina1@gmail.com>
2026-01-20 23:42:50 -0300

    HUMOBILE V0.54

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -14,7 +14,7 @@ const links = [
   { name: 'Inicio', href: '/' },
   { name: 'Quiénes Somos', href: '#QuienesSomos' },
   { name: 'Contacto', href: '/contacto' },
-  { name: 'Explorar', href: '/agenda' },
+  { name: 'Explorar', href: '/busqueda' },
   { name: 'Inicia sesión', href: '/login' },
 ];
 
