2fa256ef2e10537ea9c064f1a4b3393ca8ff6892
Bastiabm18 <andresbarriosmedina1@gmail.com>
2026-02-06 11:45:27 -0300

    HUMOBILE V0.74 pwa v6

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,18 +7,20 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Humobile | Chile',
     start_url: '/',
     display: 'standalone',
-    background_color: '#000000',
-    theme_color: '#000000',
+    background_color: '#171717',
+    theme_color: '#171717',
     icons: [
       {
         src: '/Gemini_Generated_Image_cqos2tcqos2tcqos-removebg-preview.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'maskable',
       },
       {
         src: '/Gemini_Generated_Image_cqos2tcqos2tcqos-removebg-preview.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'maskable',
       },
     ],
   }
